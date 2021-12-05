34e51778221002547400c89cf7a9b1e2fd2f71a8
abhinav <abhinavrjirapure123.com>
2021-12-05 16:30:56 +0530

    Flight Booking Eureka Service First Commit

---
+++ flight-booking-eureka-service/src/test/java/com/flight/eureka/FlightBookingEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
