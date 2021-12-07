092c2431120ca43f34910f8164281b968535cb12
abhinav <abhinavrjirapure123.com>
2021-12-07 17:49:35 +0530

    Eureka Changes

--- flight-booking-eureka-service/src/main/java/com/flight/eureka/FlightBookingEurekaServiceApplication.java
+++ flight-booking-eureka-service/src/main/java/com/flight/eureka/FlightBookingEurekaServiceApplication.java
@@ -3,11 +3,10 @@ package com.flight.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @SpringBootApplication
 @EnableEurekaServer
-@CrossOrigin(origins = "*")
 public class FlightBookingEurekaServiceApplication {
 
 	public static void main(String[] args) {
